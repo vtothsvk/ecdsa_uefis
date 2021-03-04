bef59b0f401e14c29b3068025f2ec401df587032
viktor.toth <viktor.toth@r-das.sk>
2021-03-04 23:03:13 +0100

    so far so good

--- ecdsa_uefis.ino
+++ ecdsa_uefis.ino
@@ -1,45 +1,53 @@
 #include <M5StickC.h>
 
+#include "credentials.h"
+
 #include "mbedtls/base64.h"
 #include "mbedtls/sha256.h"
+#include "mbedtls/pk.h"
+#include "mbedtls/ecdsa.h"
 
 int base64_url(uint8_t* out, size_t o_buff_size, size_t* o_len, const uint8_t* in, size_t in_len);
-int ecdsa_sign(uint8_t* out, size_t o_buff_size, size_t* o_len, const uint8_t* digest, size_t dlen);
-
-char header[200] = "{\
-  \"alg\": \"ES256\",\
-  \"typ\": \"JWT\",\
-  \"kid\": \"9802dde543434ff757166ffe23c078be41fe13779a8cdf8708bd24fb33d1318b\"\
-}";
+int ecdsa_sign(uint8_t* out, size_t o_buff_size, const uint8_t* digest, size_t dlen);
 
 void setup(void) {
     Serial.begin(115200);
 
     Serial.printf("Blink motherfucker!\r\n");
 
-    char buffer[200];
-    size_t b64_len;
+    char buffer[500];
+    size_t h64_len;
+    size_t index;
 
-    Serial.printf("63+1 encode: %d", base64_url((uint8_t*)buffer, sizeof(buffer), &b64_len, (uint8_t*)header, strlen(header)));
-    Serial.printf("output: %s\r\n", buffer);
+    Serial.printf("63+1 encode: %d", base64_url((uint8_t*)buffer, sizeof(buffer), &h64_len, (uint8_t*)header, strlen(header)));
+
+    buffer[h64_len] = '.';
+    size_t c64_len;
+    index = h64_len + 1;
+    Serial.printf("63+1 encode: %d", base64_url((uint8_t*)buffer + index, sizeof(buffer) - index, &c64_len, (uint8_t*)claim, strlen(claim)));
+    Serial.printf("output: %s\r\n", buffer);    
+
+    index += c64_len;
+    buffer[index] = '.';
+    index++;
 
     uint8_t hash[32];
     int ret = mbedtls_sha256_ret((uint8_t*)buffer, strlen(buffer), hash, NULL);
 
-    Serial.printf("hash ret: %d\r\n", ret);
-    Serial.printf("Hash hex: ");
-    for(uint8_t i = 0; i < 32; i++) {
-        Serial.printf("%2X", hash[i]);
-    }//for
-    Serial.println();
+    char signature[65];
+    ret = ecdsa_sign((uint8_t*)signature, sizeof(signature), hash, 32);
+    Serial.printf("Sign ret: %d", ret);
 
+    size_t s64_len;
+    Serial.printf("63+1 encode: %d", base64_url((uint8_t*)buffer + index, sizeof(buffer) - index, &s64_len, (uint8_t*)signature, sizeof(signature)));
+    Serial.printf("output: %s\r\n", buffer);
 }//setup
 
 void loop(void) {
     
 }//loop
 
-int base64_url(uint8_t* out, size_t o_buff_size, size_t* o_len , const uint8_t* in, size_t in_len){
+int base64_url(uint8_t* out, size_t o_buff_size, size_t* o_len, const uint8_t* in, size_t in_len){
     //size_t index;
     size_t e_len;
 
@@ -61,3 +69,38 @@ int base64_url(uint8_t* out, size_t o_buff_size, size_t* o_len , const uint8_t* 
 
     return ret;
 }//base64_url
+
+int ecdsa_sign(uint8_t* out, size_t o_buff_size, const uint8_t* digest, size_t dlen){
+
+    mbedtls_pk_context pk_ctx;
+
+    mbedtls_pk_init(&pk_ctx);
+    int ret = mbedtls_pk_parse_key(&pk_ctx, (unsigned char*)private_key, strlen(private_key) + 1, NULL, 0);
+    if (ret) return ret;
+
+    mbedtls_ecdsa_context ecdsa;
+
+    mbedtls_ecdsa_init(&ecdsa);
+    ret = mbedtls_ecdsa_from_keypair(&ecdsa, mbedtls_pk_ec(pk_ctx));
+    if (ret) return ret;
+
+    mbedtls_mpi r, s;
+    mbedtls_mpi_init(&r);
+    mbedtls_mpi_init(&s);
+
+    ret = mbedtls_ecdsa_sign_det(&ecdsa.grp, &r, &s, &ecdsa.d, digest, dlen, MBEDTLS_MD_SHA256);
+    if (ret) return ret;
+
+    ret = mbedtls_mpi_write_binary(&r, out, mbedtls_mpi_size(&r));
+    if (ret) return ret;
+
+    ret = mbedtls_mpi_write_binary(&s, out + mbedtls_mpi_size(&r), mbedtls_mpi_size(&s));
+    if(ret) return ret;
+
+    mbedtls_pk_free(&pk_ctx);
+    mbedtls_ecdsa_free(&ecdsa);
+    mbedtls_mpi_free(&r);
+    mbedtls_mpi_free(&s);
+
+    return ret;
+}//ecdsa_sign

1bd147109882e43c1b9d5aeec0ff5873c47711e6
vtothsvk <52499237+vtothsvk@users.noreply.github.com>
2021-03-05 18:50:46 +0100

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -2,18 +2,14 @@
 #define CREDENTIALS_H
 
 const char private_key[] = 
-"-----BEGIN EC PRIVATE KEY-----\n" 
-"MHcCAQEEIAMLyN6ZrlN6t1M/zExoBDa45IHLcq1wf1iEvJXJs4RFoAoGCCqGSM49\n" 
-"AwEHoUQDQgAEvRbRZdlYoNjxfNBlmz2pvKLNBx338acIc8HVVel/+tgajPoiIKdE\n" 
-"xLkEGXaN+kigf6EKgm1C/qFx6GmtDucBXg==\n" 
-"-----END EC PRIVATE KEY-----\n";
+"INSERT YOUR PRIVATE KEY HERE";
 
 
-char* kid = "73c326f9669e369662fe3ce1fabf9df32cc87eca0780795c4773fedd4f57f9b5";
-char* serial_num = "a933af5dd3b16b22";
+char* kid = "DEV KID";
+char* serial_num = "DEV SN";
 char* server_name = "https://fei.edu.r-das.sk:51415/api/v1/Auth";
-const char* ssid       = "ADB-CFF9A1";
-const char* password   = "rce6bn743cjr";
+const char* ssid       = "YOUR SSID";
+const char* password   = "YOUR PWD";
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;

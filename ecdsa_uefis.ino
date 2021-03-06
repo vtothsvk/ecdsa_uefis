71121283f54e7b350bf22ee3e4e10383bfd699fa
Simon Krajcik <simon.krajcik@sensoneo.com>
2021-03-06 11:56:17 +0100

    Final Library

--- ecdsa_uefis.ino
+++ ecdsa_uefis.ino
@@ -30,30 +30,34 @@ void setup(void) {
     }
     Serial.println("Wifi OK!");
 
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);    //Time test
-    LocalTime();  
   
+
+    HTTPClient http;                                                                                      //Initialize HTTP Client
+    http.begin(server_name);
     while(1)
     {
-      time(&time_iat);                                            //get fresh time for timestamp
-      time_exp = time_iat + sec_to_expire;
+      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);    //Time test
+      LocalTime();  
+      time(&time_iat);                                                                                    //get fresh time for timestamp
+      time_exp = time_iat + sec_to_expire;  
 
-      HTTPClient http;                                                                                      //Initialize HTTP Client
-      http.begin(server_name);
-
+      char token[500];
+      Bearer_JWT_Token(private_key, kid, serial_num, time_iat,time_exp,token);                              //Get JWT Token
+      Serial.printf("\nDebug Token -> ");
+      Serial.printf((char*) token);   
+      
       http.addHeader("Content-Type","application/json");
-      http.addHeader("Authorization",(char*)Bearer_JWT_Token(private_key, kid, serial_num, time_iat, time_exp));
-     // char* token = Bearer_JWT_Token(private_key, kid, serial_num, time_iat, time_exp);
-      //Serial.printf((char*)token);                                                                             
-      int http_response = http.POST((char*)payload);
+      http.addHeader("Authorization",token);                                                                          
+      int http_response = http.POST(payload);
       Serial.print("\n HTTP Code ");                                          
       Serial.print(http_response);
+      Serial.printf("\n\n\n");
       String server_response = http.getString();
-      Serial.print("\nServer response ");
-      Serial.print(server_response);
-      http.end();
+      //Serial.print("\nServer response ");
+      //Serial.print((char*)server_response);
       delay(1000);
     }//while
+       http.end();
 }//setup
 
 
--- jwt_lib.cpp
+++ jwt_lib.cpp
@@ -6,7 +6,7 @@
 #include "mbedtls/ecdsa.h"
 #include "jwt_lib.h"
 
-char* Bearer_JWT_Token(const char* private_key, char* kid, char* serial_num, int time_iat, int time_exp)
+int Bearer_JWT_Token(const char* private_key, char* kid, char* serial_num, int time_iat, int time_exp,char* bearer_token)
 {
     int err_ret = 0;
     char buffer[600];
@@ -18,7 +18,8 @@ char* Bearer_JWT_Token(const char* private_key, char* kid, char* serial_num, int
     char header[200];
     sprintf(header, "{\"alg\":\"ES256\",\"typ\":\"JWT\",\"kid\":\"%s\"}", kid);                             //Generate header base64_url
     err_ret = base64_url((uint8_t*)buffer, sizeof(buffer), &h64_len, (uint8_t*)header, strlen(header));
-
+    if (err_ret) return err_ret;
+    
     Serial.printf("63+1 encode: %s\r\n", buffer);
     buffer[h64_len] = '.';
     index = h64_len + 1;
@@ -26,29 +27,32 @@ char* Bearer_JWT_Token(const char* private_key, char* kid, char* serial_num, int
     char claim[200];
     sprintf(claim, "{\"sub\":\"%s\",\"iat\":%ld,\"exp\":%ld}", (char*)serial_num, (char*)time_iat, (char*)time_exp);   //Generate claim base64_url
     err_ret = base64_url((uint8_t*)buffer + index, sizeof(buffer) - index, &c64_len, (uint8_t*)claim, strlen(claim));
-
+    if (err_ret) return err_ret;
+    
     index += c64_len;
     uint8_t hash[32];
     err_ret = mbedtls_sha256_ret((uint8_t*)buffer, index, hash, NULL);                                             //Generate HASH SHA256
+    if (err_ret) return err_ret;
 
     buffer[index] = '.';
     index++;
 
     uint8_t signature[64];
     err_ret = ecdsa_sign(signature, sizeof(signature), hash, 32, private_key);
-
+    if (err_ret) return err_ret;
+    
     err_ret = base64_url((uint8_t*)buffer + index, sizeof(buffer) - index, &s64_len, signature, 64);                   //Completize JWT Token
-
-    char bearer_token[250];                                                                                            //Make Bearer Token
-    sprintf(bearer_token, "%s%s", "Bearer ", (char*)buffer);
-   // Serial.println((char*)bearer_token);
-    return bearer_token;
+    if (err_ret) return err_ret;
+    
+   // char bearer_token[250];                                                                                            //Make Bearer Token
+    sprintf((char*) bearer_token, "%s%s", "Bearer ", (char*)buffer);
+    Serial.println((char*)bearer_token);
+    return 0;
 }//Bearer_JWT_Token
 
 
 int base64_url(uint8_t* out, size_t o_buff_size, size_t* o_len, const uint8_t* in, size_t in_len) 
     {
-    //size_t index;
     size_t e_len;
 
     int ret = mbedtls_base64_encode(out, o_buff_size, &e_len, in, in_len);
